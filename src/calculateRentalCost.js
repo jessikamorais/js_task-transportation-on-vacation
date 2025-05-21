8b71ab1f2fa64c3d25c3dbe3f2c3dc8083428f91
jessika_morais <jessikanmorais@gmail.com>
2025-05-21 16:02:52 -0300

    add task solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -3,8 +3,23 @@
  *
  * @return {number}
  */
+
+const DAY_COSTS = 40;
+const SHORT_TERM = 3;
+const SHORT_TERM_DISCOUNT = 20;
+const LONG_TERM = 7;
+const LONG_TERM_DISCOUNT = 50;
+
 function calculateRentalCost(days) {
-  // write code here
+  if (days < SHORT_TERM) {
+    return days * DAY_COSTS;
+  }
+
+  if (days >= LONG_TERM) {
+    return days * DAY_COSTS - LONG_TERM_DISCOUNT;
+  }
+
+  return days * DAY_COSTS - SHORT_TERM_DISCOUNT;
 }
 
 module.exports = calculateRentalCost;
